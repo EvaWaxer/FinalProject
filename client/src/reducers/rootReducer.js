e68475fbae58e011403ca92b65593908f26fc284
EvaWaxer <hava90@gmail.com>
2020-10-04 13:28:30 +0300

    daily

--- client/src/reducers/rootReducer.js
+++ client/src/reducers/rootReducer.js
@@ -1,10 +1,14 @@
 import { combineReducers } from "redux";
 
 import fetchSchoolsReducer from "./fetchSchoolsReducer";
+import auth from "./auth";
+import message from "./message"
 //import userReducer from "./userReducer";
 
 const rootReducer = combineReducers({
   fetchSchoolsReducer,
+  auth,
+  message,
   //userReducer
 });
 
